524e3c5e8c7bbfad5c2fd2e7f537d525aa2aa0bc
Sara Smith <SSMITH9349@my.sullivan.edu>
2022-05-30 13:14:59 -0400

    Completed #2

--- CSC240-CreateFileName/CSC240-CreateFileName/Program.cs
+++ CSC240-CreateFileName/CSC240-CreateFileName/Program.cs
@@ -3,5 +3,11 @@ using System.IO;
 
 class CreateFileName
 {
+    static void Main()
+    {
+        FileStream file = new FileStream("Names.txt", 
+            FileMode.Create, FileAccess.Write);
+        StreamWriter writer = new StreamWriter(file);
 
+    }
 }
