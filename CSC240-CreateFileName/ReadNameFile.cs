e3ab4d60fc24fae5aaba42fa5bd230b0e31f1187
Sara Smith <SSMITH9349@my.sullivan.edu>
2022-05-30 21:36:51 -0400

    Completed #2

--- CSC240-CreateFileName/ReadNameFile.cs
+++ CSC240-CreateFileName/ReadNameFile.cs
@@ -9,5 +9,11 @@ public class ReadNameFile
 		FileStream file = new FileStream("Names.txt",
 				FileMode.Open, FileAccess.Read);
 	StreamReader reader = new StreamReader(file);
+
+	const int END = 999;
+	int count = 0;
+	int num;
+	int size;
+	string name;
 	}
 }
